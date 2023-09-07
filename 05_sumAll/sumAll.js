2038f2268cf32652563e271c2a54f02050ce7be0
KajetanPiwowarek <kajtek@piwowarek.com>
2023-09-07 18:21:46 +0200

    ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(numSmall, numLarge) {
+    if(numSmall < 0 || numLarge < 0) return "ERROR";
+    if (!Number.isInteger(numSmall) || !Number.isInteger(numLarge)) return "ERROR";
+    
+    if(numSmall > numLarge){
+        let tmp = numLarge;
+        numLarge = numSmall;
+        numSmall = tmp;
+    }
 
+    let sum = 0;
+    for(let i=numSmall; i<=numLarge; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
