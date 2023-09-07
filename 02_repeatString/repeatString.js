84b400a9cb7b1907cba59b422aea31df2444321d
KajetanPiwowarek <kajtek@piwowarek.com>
2023-09-07 17:34:19 +0200

    ex 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(givenString, givenNumber) {
+    if(givenNumber < 0){
+        return "ERROR";
+    }
+    let finalString = "";
+    for(let i=0; i<givenNumber; i++){
+        finalString += givenString;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
